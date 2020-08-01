cc6b67151bd424d04cd00229eb43c402b6448442
dawei-Z <sgdzhou5@gmail.com>
2020-08-01 23:42:25 +0800

    remove bert model

--- util/Data_Harvest.py
+++ util/Data_Harvest.py
@@ -1,5 +1,10 @@
 from praw import Reddit
 from praw.models import MoreComments
+import json
+from datetime import datetime
+import requests
+import time
+from urllib.request import urlopen
 import pandas as pd
 import os
 
@@ -9,36 +14,60 @@ class InstanceLogin:
         """Generate reddit instance"""
         self.reddit = Reddit(user_agent='Comment Extraction (by /u/sgdzhou5)',
                              client_id='zanmra52bp9GSg', client_secret='jrm-DL_IxEexh8WZbi1VduOmAFk')
+        self.start_time = datetime.utcnow()
 
+    def submission_extraction(self):
+        url = "https://api.pushshift.io/reddit/submission/search?subreddit=iot&before={}&sort=desc&size=1000"
+        count = 0
+        id_ls = []
+        previous_epoch = int(self.start_time.timestamp())
+        while True:
+            new_url = url.format(str(previous_epoch))
+            json = requests.get(new_url, headers={'User-Agent': 'Comment Extraction (by /u/sgdzhou5)'})
+            time.sleep(1)
+            json_data = json.json()
+            if 'data' not in json_data:
+                break
+            objects = json_data['data']
+            if len(objects) == 0:
+                break
+            for object in objects:
+                previous_epoch = object['created_utc'] - 1
+                count += 1
+                if object['is_self']:
+                    if 'selftext' not in object:
+                        continue
+                    try:
+                         id_ls.append(object['id'])
+                    except Exception as err:
+                        print(f"Couldn't print post: {object['url']}")
+                print("Saved {} submissions through {}.".format(count, datetime.fromtimestamp(previous_epoch).strftime("%Y-%m-%d")))
+        print(f"Saved {count}")
+        return id_ls
 
-    def extract_comment(self, forum, filter, corpora):
+    def extract_comment(self, id_ls):
         """Harvest comment data from Reddit"""
-        subreddit = ""
-        corpus = pd.DataFrame(columns=["Title", "Comments"])
-        if filter == "top":
-            subreddit = self.reddit.subreddit(forum).top('all')
-        elif filter == "new":
-            subreddit = self.reddit.subreddit(forum).new()
-        elif filter == "hot":
-            subreddit = self.reddit.subreddit(forum).hot()
-        for submission in subreddit:
+        corpus = pd.DataFrame(columns=["Title", "Comment"])
+        for i in id_ls:
+            submission = self.reddit.submission(id=i)
+            submission.comments.replace_more(limit=None)
             title = submission.title
-            if title not in corpora['Title'].values:
+            comment_queue = submission.comments[:]
+            while comment_queue:
+                comment = comment_queue.pop(0)
                 print(title)
-                for top_level_comment in submission.comments:
-                    if isinstance(top_level_comment, MoreComments):
-                        comments = top_level_comment.comments()
-                    else:
-                        comments = [top_level_comment.body]
-                    for i in comments:
-                        d = {"Title": [title], "Comments": [i]}
-                        corpus = pd.concat([corpus, pd.DataFrame(d)],
-                                           ignore_index=True)
+                temp = comment.body
+                d = {"Title": title, "Comment": temp}
+                corpus = corpus.append(d, ignore_index=True)
+                comment_queue.extend(comment.replies)
         return corpus
 
 
 path = os.path.dirname(os.path.dirname(__file__)) + '/Corpora/'
-for i in ["top", "hot", "new"]:
-    corpora = pd.read_csv(path + "{}.csv".format(i))
-    corpus = InstanceLogin().extract_comment('IoT', i, corpora)
-    corpus.to_csv(path + "{}[0711].csv".format(i), index=False)
+# for i in ["top", "hot", "new"]:
+#     corpora = pd.read_csv(path + "{}.csv".format(i))
+reddit = InstanceLogin()
+id_ls = reddit.submission_extraction()
+corpus = InstanceLogin().extract_comment(id_ls)
+corpus.to_csv(path + "new_corpus.csv", index=False)
+
--- util/Data_Preprocess.py
+++ util/Data_Preprocess.py
@@ -1,10 +1,18 @@
 import os
 import pandas as pd
 import re
+from spellchecker import SpellChecker
+import nltk
+from gensim.utils import simple_preprocess
+from nltk.corpus import stopwords
+import sys
+
+
+path = os.path.dirname(os.path.dirname(__file__)) + '/Corpora/'
 
 
 def load_data(file_name, date):
-    path = os.path.dirname(os.path.dirname(__file__)) + '/Corpora/'
+
     corpora = pd.DataFrame()
     for i in file_name:
         for j in date:
@@ -14,23 +22,79 @@ def load_data(file_name, date):
 
 class DataPreprocess:
     def __init__(self):
-        self.regx_ls = [re.compile(r'([^\s\w])+'),  # keep only alphanumeric characters and spaces.
+        self.regx_ls = [re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)))'),  # remove urls
+                        re.compile(r'[^A-Za-z0-9\s ]'),  # keep only alphanumeric characters and spaces.
                         re.compile(r'\b(\w)\b'),  # remove words with only 1 character
                         re.compile(r'\b(\d+)\b'),  # remove number that are fully made of digit
-                        re.compile(r'\b(\w+://\S*)\b'),  # remove urls
                         re.compile(r'\b([@|#]\w*)\b')]  # remove hash tag and at
+        self.unwords = 0
 
-    def regx_parse(self, raw_text):
-        for i in range(len(raw_text)):
+    def preprocess_pipeline(self, raw_text):
+        for i in ['Title', 'Comment']:
+            raw_text[i] = self._stopwords_removal(raw_text[i])
             for j in self.regx_ls:
-                raw_text[i]['Title'] = self._regx_replace(j, raw_text[i]["Title"])
-                raw_text[i]['Comments'] = self._regx_replace(j, raw_text[i]["Comments"])
+                raw_text[i] = raw_text[i].lower()
+                raw_text[i] = self._regx_replace(j, raw_text[i])
+            raw_text[i] = self._spell_check(raw_text[i])
+            raw_text[i] = self._lemmatise(raw_text[i])
+            raw_text[i] = ' '.join(raw_text[i])
+
         return raw_text
 
     def _regx_replace(self, pattern, content):
         content = pattern.sub('', content)
         return content
 
+    def _lemmatise(self, content):
+        clean = []
+        lemmatiser = nltk.stem.WordNetLemmatizer()
+        tokens = self._tokenization(content)
+        tags = nltk.pos_tag(tokens)
+        for word, tag in tags:
+            tag = self._tag_conversion(tag)
+            if tag:
+                clean.append(lemmatiser.lemmatize(word, tag))
+            else:
+                clean.append(word)
+        return clean
 
-corpus = load_data(['hot', 'new', 'top'], ['[0628]', '[0709]', '[0422]'])
-regx_corpus = DataPreprocess().regx_parse(corpus)
+    def _tokenization(self, content):
+        return nltk.tokenize.word_tokenize(content)
+
+    def _tag_conversion(self, tag):
+        """convert POS tag to lemmatiser tag"""
+        tag = tag[0]
+        wn = nltk.corpus.wordnet
+        tag_dict = {'J': wn.ADJ,
+                    'V': wn.VERB,
+                    'N': wn.NOUN,
+                    'R': wn.ADV}
+        return tag_dict.get(tag, None)
+
+    def _spell_check(self, words):
+        spell = SpellChecker()
+        tokens = self._tokenization(words)
+        misspelled = spell.unknown(tokens)
+        if misspelled:
+            for i in misspelled:
+                # print("Unknown: {}".format(i))
+                # print("Correction: {}".format(spell.correction(i)))
+                # value = input("Correct?\n")
+                # if value == "y":
+                words.replace(i, spell.correction(i))
+                self.unwords += 1
+        return words
+
+    def _stopwords_removal(self, content):
+        stop_words = stopwords.words('english')
+        return ' '.join([word for word in simple_preprocess(str(content)) if word not in stop_words])
+
+
+corpus = pd.read_csv(path+'new_corpus.csv')
+corpus = corpus.drop_duplicates()
+print(len(corpus.drop_duplicates(["Title"])))
+print(len(corpus))
+dp = DataPreprocess()
+corpus = corpus.apply(dp.preprocess_pipeline, axis=1)
+print(dp.unwords)
+corpus.to_csv(path+'new_clean_corpora.csv')
